ca743ccd6fb8e8f3c39560f95229a83f916751b6
Nick Wells <wells.nicholas@gmail.com>
2019-01-08 19:40:55 +0000

    add package-level doc

---
+++ attempt/doc.go
@@ -0,0 +1,9 @@
+/*
+
+Package attempt offers funcs that let you try to run a function up to a
+maximum number of times, repeating if the function returns an error. You can
+also specify the interval between attempts by passing a Waiter which will
+calculate the next interval between attempts.
+
+*/
+package attempt
